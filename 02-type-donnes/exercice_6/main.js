3454cda8fcfa75815da76a6260a53910e3cb884d
paulvictor <paulndjoku22@gmail.com>
2025-10-16 15:11:39 +0200

    Additionning test typedonnes

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,8 +1,9 @@
 // Exercice 6
-let age = 25; 
-let nom = "Paul"; 
-let estConnecte = true; 
-let valeurNulle = null; 
-let inconnue;
-module.exports = { age, nom, estConnecte, valeurNulle, inconnue };
+// Exercice 6 : déclarer les variables demandées
+
+let age = 25;                // nombre
+let nom = "Paul";            // chaîne
+let estConnecte = true;      // booléen
+let valeurNulle = null;      // null
+
 
