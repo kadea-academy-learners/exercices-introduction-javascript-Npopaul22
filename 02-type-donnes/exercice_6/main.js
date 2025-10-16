d342afa9204fdcb900ec8de32f5b8b45b1eaa282
paulvictor <paulndjoku22@gmail.com>
2025-10-16 12:04:31 +0200

    solution for exercises 6

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,2 +1,8 @@
 // Exercice 6
-// Écrivez votre code ici
+let age = 25; 
+let nom = "Paul"; 
+let estConnecte = true; 
+let valeurNulle = null; 
+let inconnue;
+module.exports = { age, nom, estConnecte, valeurNulle, inconnue };
+
