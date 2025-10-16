36fb9d7b71add2fa416acc373af1998aa34254a2
paulvictor <paulndjoku22@gmail.com>
2025-10-16 14:18:21 +0200

    Correction after Test1

--- 01-variables/exercice_4/main.js
+++ 01-variables/exercice_4/main.js
@@ -1,10 +1,10 @@
 // Déclaration des variables
-let a = 5;
-let b = 8;
+const a = 5;
+const b = 8;
 
 // Affichage de la somme
 console.log(a + b); 
 
-gi// Résultat : 13
+// Résultat : 13
 // let somme = a + b;
 // console.log(somme);
